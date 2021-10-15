3e54fe6d9b380a36a731e1e6ef997498cdd389dd
karthik <radhakarthik07@gmail.com>
2021-10-15 17:56:34 +0530

    successfull

---
+++ index.js
@@ -0,0 +1,15 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 44D9-4DEC
+
+ Directory of C:\Users\KARTHI\OneDrive\Desktop\karthikjava
+
+15-10-2021  17:55    <DIR>          .
+15-10-2021  17:55    <DIR>          ..
+15-10-2021  17:49               454 Hello.class
+15-10-2021  17:14               130 Hello.java
+15-10-2021  17:55                 0 index.js
+14-10-2021  18:36               190 pathdetails.txt
+15-10-2021  10:45               130 project1.txt
+15-10-2021  17:54               561 readme.txt
+               6 File(s)          1,465 bytes
+               2 Dir(s)  128,905,953,280 bytes free
---
+++ readme.txt
@@ -0,0 +1,14 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 44D9-4DEC
+
+ Directory of C:\Users\KARTHI\OneDrive\Desktop\karthikjava
+
+15-10-2021  17:54    <DIR>          .
+15-10-2021  17:54    <DIR>          ..
+15-10-2021  17:49               454 Hello.class
+15-10-2021  17:14               130 Hello.java
+14-10-2021  18:36               190 pathdetails.txt
+15-10-2021  10:45               130 project1.txt
+15-10-2021  17:54                 0 readme.txt
+               5 File(s)            904 bytes
+               2 Dir(s)  128,906,493,952 bytes free
